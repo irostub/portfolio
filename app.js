8d1b0bfe0767f9e1258655762654bf0ed3877ef0
irostub <irostub@gmail.com>
2021-09-17 18:51:02 +0900

    Create README.md

--- app.js
+++
@@ -1,7 +0,0 @@
-const docHeight = document.body.clientHeight;
-const viewHeight = window.innerHeight;
-
-document.addEventListener('scroll', () => {
-    let nowScroll = window.scrollY;
-    document.getElementById("scrollbar").style.setProperty("--scroll-value", 100 * (nowScroll / (docHeight - viewHeight)) + '%');
-});
